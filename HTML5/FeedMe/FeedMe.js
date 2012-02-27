3acc3458f131ca7c72342f1fbf3b4027b5a65cec
eterna2 <eterna2@hotmail.com>
2012-02-27 19:07:11 +0800

    Signed-off-by: eterna2 <eterna2@hotmail.com>

--- HTML5/FeedMe/FeedMe.js
+++ HTML5/FeedMe/FeedMe.js
@@ -158,6 +158,7 @@ var Update = function(){
 	dh = dollars.height/2,
 	dmin = w*w+h*h,
 	d = 0,
+	dd =0,
 	min = -1,
 	sumx = 0,
 	sumy = 0;
@@ -184,8 +185,20 @@ var Update = function(){
 		dy = food[i+1] - sumy;
 		d = dx*dx+dy*dy;
 
+		dx = sumx - x,
+		dy = sumy - y;
+		d += dx*dx+dy*dy;
 		
-		if (dmin > d)
+		dx = food[i] - x,
+		dy = food[i+1] - y;
+		dd = dx*dx+dy*dy;
+		
+		if (dd <= iw*iw) 
+		{
+			dmin = dd;
+			min = i;
+		}
+		else if (dmin > d)
 		{
 			dmin = d;
 			min = i;
